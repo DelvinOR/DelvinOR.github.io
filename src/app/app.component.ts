a15d33f8645737e454823d6afd35c801790f6357
Delvin Ortiz <delvin732@gmail.com>
2020-12-17 19:23:32 -0500

    Display flex property added to parent div element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template:`
-    <app-left [class] = "appLeftClass"></app-left>
+  <div [class] = "displayClass">
+    <app-left></app-left>
     <app-right></app-right>
+  </div>
   `,
   styles: [`
-    .left{
-      float:left
+    .disp-flex{
+      display: flex;
+      background-color: #F0F8FF
     }
   `]
 })
 export class AppComponent {
   public title = 'Delvin Ortiz';
-  public appLeftClass = ".left"
+  public displayClass = ".disp-flex"
 }
