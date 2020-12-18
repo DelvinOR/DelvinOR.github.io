71e5b8dbc414c583659c0cf001557dd9880f7f50
Delvin Ortiz <delvin732@gmail.com>
2020-12-17 19:44:46 -0500

    Flex and backgroun property added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,23 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template:`
-  <div [class] = "displayClass">
-    <app-left class = "left"></app-left>
-    <app-right class = "right"></app-right>
-  </div>
+  <!DOCTYPE html>
+  <html lang="en">
+
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Delvin Ortiz</title>
+  </head>
+
+  <body style = "background-color: #F0F8FF;">
+    <div [class] = "displayClass">
+      <app-left class = "left"></app-left>
+      <app-right class = "right"></app-right>
+    </div>
+  </body>
+
+  </html>
   `,
   styles: [`
     .disp-container{
       display: flex;
       justify-content: space-between;
-      background-color: #F0F8FF;
     }
 
     .left{
+      border-style: dotted;
       flex-grow: 1;
     }
 
     .right{
+      border-style: dotted;
       flex-grow: 1;
     }
   `]
