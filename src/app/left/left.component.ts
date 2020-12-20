5e10c9c42fc63500eb9489318d1b37db15380136
Delvin Ortiz <delvin732@gmail.com>
2020-12-19 21:45:28 -0500

    border along img tag

--- src/app/left/left.component.ts
+++ src/app/left/left.component.ts
@@ -9,10 +9,12 @@ import { Component, OnInit } from '@angular/core';
   <br>
   <img class = "center" src = "assets/images/profile_headshot_shot.png" alt = "Profile headshot">
   <br>
-  <p class = "paragraph text">I am a recent graduate from Rutgers University. Currently, I aspire 
+  <br>
+  <p class = "paragraph text">I am a recent graduate from Rutgers University. I aspire 
     to obtain a position in software engineering or application development and 
     to one day work in the space industry.
   </p>
+  <br>
   <p class = "paragraph text">Fun fact: In 2019. I traveled to Phoenix, AZ and enjoyed In N Out 
     for the first time. My favorite animated series is Robotech (1985).
   </p>
@@ -40,6 +42,7 @@ import { Component, OnInit } from '@angular/core';
       margin: auto;
       width: 300px;
       height: 286px;
+      border: 1px solid #D3D3D3;
       border-radius: 50%;
     }
 
