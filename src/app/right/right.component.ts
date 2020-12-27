851b9b771eb6f1b916bb97f2788b3ca15d642992
Delvin Ortiz <delvin732@gmail.com>
2020-12-26 23:39:43 -0500

    Hr color update

--- src/app/right/right.component.ts
+++ src/app/right/right.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   template:`
 
   <h2 class = "category">Portfolio</h2>
-  <hr style = "border-color:#000000">
+  <hr style = "border-color:#00FFFF">
 
   <h2 class = "project-type">Android App</h2>
   <a class = "project" href = "https://github.com/DelvinOR/distance-conversion-app" target = "_blank">Distance Converter: application used to change units of distance</a>
@@ -19,7 +19,7 @@ import { Component, OnInit } from '@angular/core';
     <img class = "picture" src = "assets/images/project_1/Screenshot_three.png" alt = "Project 1 screenshot_3">
   </div>
   <br>
-  <hr style = "border-color:#000000">
+  <hr style = "border-color:#00FFFF">
 
   <h2 class = "project-type">Android App</h2>
   <a class = "project" href = "https://github.com/DelvinOR/android-chess-game" target = "_blank">Chess Game App: android chess app with screen recording functionality </a>
@@ -35,7 +35,7 @@ import { Component, OnInit } from '@angular/core';
     <img class = "picture" src = "assets/images/project_2/Screenshot_four.png" alt = "Project 2 screenshot four">    
   </div>
   <br>
-  <hr style = "border-color:#000000">
+  <hr style = "border-color:#00FFFF">
 
   <h2 class = "project-type">eCommerce Website</h2>
   <a class = "project" href = "http://www.melanatedmarketcommerce.org/#/" target = "_blank">Melanated Market Commerce: multi-page eCommerce site with product listing, contact form, and mailing list</a>
