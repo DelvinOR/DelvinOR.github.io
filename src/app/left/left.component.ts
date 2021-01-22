59b0e2cd9abf6b5d64c647278a9aa996e46c2555
Delvin Ortiz <delvin732@gmail.com>
2021-01-22 14:13:57 -0500

    Description update

--- src/app/left/left.component.ts
+++ src/app/left/left.component.ts
@@ -14,7 +14,7 @@ import { Component, OnInit } from '@angular/core';
   <br>
   <br>
   <p class = "paragraph text">Four years of programming experience. Most proficient 
-  technologies include Java, HTML, CSS, JavaScript, SQL, and Git. Knowledgeable with Linux Ubuntu and terminal commands. 
+  technologies include Java, HTML, CSS, JavaScript, SQL, and Git. 
   Posses a Bachelor of Science degree in Computer Science from Rutgers University. 
   </p>
   <p class = "paragraph text">Fun fact: This website was built over the Angular framework and utilizes the Angular CLI.
