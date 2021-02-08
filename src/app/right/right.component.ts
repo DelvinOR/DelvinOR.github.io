2130e5a5aa3afdae52b1ffd7e95dc610f03dbf67
Delvin Ortiz <delvin732@gmail.com>
2021-02-07 21:34:11 -0500

    Project description update

--- src/app/right/right.component.ts
+++ src/app/right/right.component.ts
@@ -111,7 +111,7 @@ import { Component, OnInit } from '@angular/core';
     paragraph {
       font-size: 100%;
       font-family: "Times New Roman", sans-serif;
-      color: #F8F8FF;
+      text-color: #F8F8FF;
     }
 
     .pictures{
